7b0f01912145617467ed3b0f9206d8809893801e
Devs Deck <69584123+prashanthmani90@users.noreply.github.com>
2023-03-05 15:31:10 +0000

    One more update

--- tests/index.get.test.js
+++ tests/index.get.test.js
@@ -6,16 +6,19 @@ const { default: parse } = require("node-html-parser")
 describe("HomePage", () => {
     it("should render an HTML form", async () => {
         const response = await supertest(app).get("/")
-        expect(response.status).toBe(200)
-
+        expect(response.status).toBe(200) // Check the status to be 200
         expect(response.type).toBe("text/html")
         const root = parse(response.text)
-        const input = root.querySelector('input[type="text"][name="username"]')
         const form = root.querySelector("form")
-        const button = root.querySelector("button")
-        expect(form).toBeTruthy()
-        expect(input).toBeTruthy()
-        expect(button).toBeTruthy()
-        expect(response).toMatchSnapshot()
+        const input = root.querySelector('input[type="text"][name="username"]')
+        const button = root.querySelector('button[type="submit"]')
+        expect(form).toBeTruthy() // Validate form
+        expect(input).toBeTruthy() // Validate form contains an input
+        expect(button).toBeTruthy() // validate form have a submit button
+        const template = pug.compileFile("./views/form.pug")
+        const renderedHtml = template({
+            title: `Find Github User's Dominatin Programming Language`
+        })
+        expect(renderedHtml).toMatchSnapshot()
     })
 })
